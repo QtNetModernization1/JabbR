5e6b7e1ad02cc4f393be4cb49f301178a878c682
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:19:21 +0000

    [Commit] Iteration 65 #errors = 15411 <= 15411.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Properties/AssemblyInfo.cs][CS0433][7,12] The type 'GuidAttribute' exists in both 'mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' and 'System.Runtime.InteropServices, Version=4.1.1.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'``.

--- JabbR/Properties/AssemblyInfo.cs
+++ JabbR/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
+[assembly: System.Runtime.InteropServices.Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
