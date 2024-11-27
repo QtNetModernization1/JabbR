194fd3a284fb99255a32bfa698132005c264dae7
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:33:34 +0000

    [Commit] Iteration 163 #errors = 40 <= 40.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs][CS0534][18,18] 'JabbRNinjectNancyBootstrapper' does not implement inherited abstract member 'NancyBootstrapperBase<IKernel>.GetEnvironmentConfigurator()'``.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -10,6 +10,7 @@ using Nancy.Bootstrapper;
 using Nancy.Bootstrappers.Ninject;
 using Nancy.Configuration;
 using Nancy.Owin;
+using Nancy.TinyIoc;
 
 using Ninject;
 
@@ -22,6 +23,11 @@ namespace JabbR.Nancy
         public JabbRNinjectNancyBootstrapper(IKernel kernel)
         {
             _kernel = kernel;
+        }
+
+        protected override Func<ITypeCatalog, NancyInternalConfiguration> GetEnvironmentConfigurator()
+        {
+            return NancyInternalConfiguration.Default;
         }
 
         public interface IUserIdentity
