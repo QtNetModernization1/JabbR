4d48e22d08453cd76e99d309043d7ad6de473ad8
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:01:46 +0000

    Revert "[Commit] Iteration 18 #errors = 15431 <= 206."
    This reverts commit 8bd1f9e2a82ec3b9edeb7eaad4c195040cdcf346.

--- JabbR/ContentProviders/Core/CollapsibleContentProvider.cs
+++ JabbR/ContentProviders/Core/CollapsibleContentProvider.cs
@@ -1,10 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Net;
+using Microsoft.Security.Application;
 
 namespace JabbR.ContentProviders.Core
 {
@@ -12,13 +12,12 @@ namespace JabbR.ContentProviders.Core
     {
         public virtual Task<ContentProviderResult> GetContent(ContentProviderHttpRequest request)
         {
-            return GetCollapsibleContent(request).ContinueWith(task =>
+            return GetCollapsibleContent(request).Then(result =>
             {
-                var result = task.Result;
                 if (GetIsCollapsible(request.RequestUri) && result != null)
                 {
-                    string contentTitle = string.Format(LanguageResources.Content_HeaderAndToggle, WebUtility.HtmlEncode(result.Title));
-                    result.Content = string.Format(ContentFormat, contentTitle, result.Content);
+                    string contentTitle = String.Format(LanguageResources.Content_HeaderAndToggle, Encoder.HtmlEncode(result.Title));
+                    result.Content = String.Format(ContentFormat, contentTitle, result.Content);
                 }
 
                 return result;
