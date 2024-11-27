8748b25dc86eb0b2b7bc7e849f0a949ab5d1b3c8
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:22:05 +0000

    Revert "[Commit] Iteration 71 #errors = 15409 <= 15409."
    This reverts commit f6e6db4c5a1c5bf69fd2235cc18bcb6bb5dd77ed.

--- JabbR/Properties/AssemblyInfo.cs
+++ JabbR/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 // This file is now mostly empty as assembly attributes are handled by the SDK-style project file
 // You can add any custom attributes here if needed
 
-using System;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
