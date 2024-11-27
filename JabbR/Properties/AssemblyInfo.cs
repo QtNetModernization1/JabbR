3764d5ebf46ff57923f48e54ae8f5e2e83556a1a
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:16:18 +0000

    [Commit] Iteration 58 #errors = 15431 <= 15431.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Properties/AssemblyInfo.cs][CS0433][7,12] The type 'AssemblyTitleAttribute' exists in both 'mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' and 'System.Runtime, Version=4.1.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'``.

--- JabbR/Properties/AssemblyInfo.cs
+++ JabbR/Properties/AssemblyInfo.cs
@@ -1,25 +1,26 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Chat")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Chat")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: System.Reflection.AssemblyTitle("Chat")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCompany("Microsoft")]
+[assembly: System.Reflection.AssemblyProduct("Chat")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Reflection.AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
+[assembly: System.Runtime.InteropServices.Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.*")]
+[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
