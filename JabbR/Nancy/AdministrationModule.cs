33dfcc0bc1585ae29cd6da92fd5106848495efe1
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:13:03 +0000

    [Commit] Iteration 120 #errors = 68 <= 66.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/AdministrationModule.cs][CS0021][38,13] Cannot apply indexing with [] to an expression of type 'method group'``.

--- JabbR/Nancy/AdministrationModule.cs
+++ JabbR/Nancy/AdministrationModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,6 +17,7 @@ namespace JabbR.Nancy
                                     IEnumerable<IContentProvider> contentProviders)
             : base("/administration")
         {
+            Get("/", parameters => Get["/"].Invoke(parameters));
             Get["/"] = _ =>
             {
                 if (!IsAuthenticated || !Principal.HasClaim(JabbRClaimTypes.Admin))
@@ -35,6 +36,7 @@ namespace JabbR.Nancy
                 return View["index", model];
             };
 
+            Post("/", parameters => Post["/"].Invoke(parameters));
             Post["/"] = _ =>
             {
                 if (!HasValidCsrfTokenOrSecHeader)
