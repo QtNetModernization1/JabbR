bd6fbf35926fdaf070535ba45a2069cfba36b664
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:12:11 +0000

    Revert "[Commit] Iteration 48 #errors = 15425 <= 206."
    This reverts commit 05bb3ffd3553d27a5f355cedd9cb417c236aa12f.

--- JabbR/Infrastructure/NinjectWebApiDependencyResolver.cs
+++ JabbR/Infrastructure/NinjectWebApiDependencyResolver.cs
@@ -1,14 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using Microsoft.Extensions.DependencyInjection;
+using System.Web.Http.Dependencies;
 using Ninject;
 using Ninject.Syntax;
 
 
 namespace JabbR.Infrastructure
 {
-public class NinjectDependencyScope : IServiceScope
+    public class NinjectDependencyScope : IDependencyScope
     {
         private IResolutionRoot resolver;
 
@@ -28,18 +28,24 @@ public class NinjectDependencyScope : IServiceScope
             resolver = null;
         }
 
-    public object GetService(Type serviceType)
-    {
-        if (resolver == null)
-            throw new ObjectDisposedException("this", "This scope has already been disposed");
+        public object GetService(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
 
-        return resolver.TryGet(serviceType);
+            return resolver.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            if (resolver == null)
+                throw new ObjectDisposedException("this", "This scope has already been disposed");
+
+            return resolver.GetAll(serviceType);
+        }
     }
 
-    public IServiceProvider ServiceProvider => this;
-    }
-
-public class NinjectWebApiDependencyResolver : NinjectDependencyScope, IServiceProvider
+    public class NinjectWebApiDependencyResolver : NinjectDependencyScope, IDependencyResolver
     {
         private IKernel kernel;
 
@@ -49,7 +55,7 @@ public class NinjectWebApiDependencyResolver : NinjectDependencyScope, IServiceP
             this.kernel = kernel;
         }
 
-public IServiceScope CreateScope()
+        public IDependencyScope BeginScope()
         {
             return new NinjectDependencyScope(kernel.BeginBlock());
         }
