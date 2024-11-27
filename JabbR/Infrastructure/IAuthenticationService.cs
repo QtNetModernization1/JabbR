c4aab57aae1bbdab61c9914c4a73bc3df9753b7a
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:10:32 +0000

    [Commit] Iteration 44 #errors = 15435 <= 206.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/IAuthenticationService.cs][CS0246][3,7] The type or namespace name 'SimpleAuthentication' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Infrastructure/IAuthenticationService.cs
+++ JabbR/Infrastructure/IAuthenticationService.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 using SimpleAuthentication.Core;
+using SimpleAuthentication.Core.Providers;
 
 namespace JabbR.Infrastructure
 {
