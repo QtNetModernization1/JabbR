f6e6db4c5a1c5bf69fd2235cc18bcb6bb5dd77ed
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:22:05 +0000

    [Commit] Iteration 71 #errors = 15409 <= 15409.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Properties/AssemblyInfo.cs][CS0518][6,23] Predefined type 'System.Boolean' is not defined or imported``.

--- JabbR/Properties/AssemblyInfo.cs
+++ JabbR/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 // This file is now mostly empty as assembly attributes are handled by the SDK-style project file
 // You can add any custom attributes here if needed
 
+using System;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
