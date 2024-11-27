8e058792970a566c8c896bae344293e34d7c0ad5
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:16:18 +0000

    Revert "[Commit] Iteration 58 #errors = 15431 <= 15431."
    This reverts commit 3764d5ebf46ff57923f48e54ae8f5e2e83556a1a.

--- JabbR/Properties/AssemblyInfo.cs
+++ JabbR/Properties/AssemblyInfo.cs
@@ -1,26 +1,25 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: System.Reflection.AssemblyTitle("Chat")]
-[assembly: System.Reflection.AssemblyDescription("")]
-[assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: System.Reflection.AssemblyCompany("Microsoft")]
-[assembly: System.Reflection.AssemblyProduct("Chat")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright © Microsoft 2011")]
-[assembly: System.Reflection.AssemblyTrademark("")]
-[assembly: System.Reflection.AssemblyCulture("")]
+[assembly: AssemblyTitle("Chat")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Chat")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: System.Runtime.InteropServices.Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
+[assembly: Guid("a51af1a7-9f3c-4d04-b9f9-1a32916e29fc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +30,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-[assembly: System.Reflection.AssemblyVersion("1.0.*")]
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
