16b5219e14ccb14a5dc510695d74ee10fb325ada
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:30:15 +0000

    [Commit] Iteration 49 #errors = 107 <= 107.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/InputExtensions.cs][CS0246][10,49] The type or namespace name 'HtmlHelpers<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/InputExtensions.cs
+++ JabbR/Nancy/InputExtensions.cs
@@ -2,50 +2,51 @@ using System;
 using System.Linq;
 using Nancy.ViewEngines.Razor;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace JabbR
 {
     public static class InputExtensions
     {
-public static IHtmlContent TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+public static IHtmlContent TextBox<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName)
         {
             return TextBox(htmlHelper, propertyName, String.Empty);
         }
 
-public static IHtmlContent TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
+public static IHtmlContent TextBox<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName, string className)
         {
             return TextBox(htmlHelper, propertyName, className, null);
         }
 
-public static IHtmlContent TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
+public static IHtmlContent TextBox<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName, string className, string placeholder)
         {
             return InputHelper(htmlHelper, "text", propertyName, htmlHelper.GetValueForProperty(propertyName), className, placeholder);
         }
 
-public static IHtmlContent Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+public static IHtmlContent Password<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName)
         {
             return Password(htmlHelper, propertyName, String.Empty);
         }
 
-public static IHtmlContent Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
+public static IHtmlContent Password<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName, string className)
         {
             return Password(htmlHelper, propertyName, className, null);
         }
 
-public static IHtmlContent Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
+public static IHtmlContent Password<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName, string className, string placeholder)
         {
             return InputHelper(htmlHelper, "password", propertyName, null, className, placeholder);
         }
 
         private const string InputTemplate = @"<input type=""{0}"" id=""{1}"" name=""{2}"" value=""{3}"" class=""{4}"" placeholder=""{5}"" />";
-private static IHtmlContent InputHelper<TModel>(HtmlHelpers<TModel> htmlHelper, string inputType, string propertyName, string value, string className, string placeholder)
+private static IHtmlContent InputHelper<TModel>(IHtmlHelper<TModel> htmlHelper, string inputType, string propertyName, string value, string className, string placeholder)
         {
             bool hasError = htmlHelper.GetErrorsForProperty(propertyName).Any();
 
             return new NonEncodedHtmlString(String.Format(InputTemplate, inputType, propertyName, propertyName, value, hasError ? String.Format("{0} {1}", className, "error").Trim() : className, placeholder));
         }
 
-        internal static string GetValueForProperty<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+        internal static string GetValueForProperty<TModel>(this IHtmlHelper<TModel> htmlHelper, string propertyName)
         {
             var propInfo =
                 typeof (TModel).GetProperties()
