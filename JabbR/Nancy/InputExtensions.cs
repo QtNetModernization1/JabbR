a2b47a7af56a6a70a13786c6eae94bdf484c2951
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:23:12 +0000

    [Commit] Iteration 40 #errors = 116 <= 116.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/InputExtensions.cs][CS0246][9,56] The type or namespace name 'HtmlHelpers<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/InputExtensions.cs
+++ JabbR/Nancy/InputExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using Nancy.ViewEngines.Razor;
 
@@ -6,45 +6,45 @@ namespace JabbR
 {
     public static class InputExtensions
     {
-        public static IHtmlString TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+        public static IHtmlString TextBox<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName)
         {
             return TextBox(htmlHelper, propertyName, String.Empty);
         }
 
-        public static IHtmlString TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
+        public static IHtmlString TextBox<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
         {
             return TextBox(htmlHelper, propertyName, className, null);
         }
 
-        public static IHtmlString TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
+        public static IHtmlString TextBox<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
         {
             return InputHelper(htmlHelper, "text", propertyName, htmlHelper.GetValueForProperty(propertyName), className, placeholder);
         }
 
-        public static IHtmlString Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+        public static IHtmlString Password<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName)
         {
             return Password(htmlHelper, propertyName, String.Empty);
         }
 
-        public static IHtmlString Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
+        public static IHtmlString Password<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName, string className)
         {
             return Password(htmlHelper, propertyName, className, null);
         }
 
-        public static IHtmlString Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
+        public static IHtmlString Password<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
         {
             return InputHelper(htmlHelper, "password", propertyName, null, className, placeholder);
         }
 
         private const string InputTemplate = @"<input type=""{0}"" id=""{1}"" name=""{2}"" value=""{3}"" class=""{4}"" placeholder=""{5}"" />";
-        private static IHtmlString InputHelper<TModel>(HtmlHelpers<TModel> htmlHelper, string inputType, string propertyName, string value, string className, string placeholder)
+        private static IHtmlString InputHelper<TModel>(Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string inputType, string propertyName, string value, string className, string placeholder)
         {
             bool hasError = htmlHelper.GetErrorsForProperty(propertyName).Any();
 
             return new NonEncodedHtmlString(String.Format(InputTemplate, inputType, propertyName, propertyName, value, hasError ? String.Format("{0} {1}", className, "error").Trim() : className, placeholder));
         }
 
-        internal static string GetValueForProperty<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+        internal static string GetValueForProperty<TModel>(this Nancy.ViewEngines.Razor.HtmlHelpers<TModel> htmlHelper, string propertyName)
         {
             var propInfo =
                 typeof (TModel).GetProperties()
