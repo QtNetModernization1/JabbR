656e33c16baf98c4b8286daf07e0203abb550b58
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:22:00 +0000

    [Commit] Iteration 38 #errors = 119 <= 120.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/JabbRDataProtection.cs][CS0535][11,40] 'JabbRDataProtection' does not implement interface member 'IDataProtectionProvider.CreateProtector(string)'``.

--- JabbR/Infrastructure/JabbRDataProtection.cs
+++ JabbR/Infrastructure/JabbRDataProtection.cs
@@ -5,20 +5,40 @@ using System.Web;
 using JabbR.Services;
 using Microsoft.AspNetCore.DataProtection;
 
-
 namespace JabbR.Infrastructure
 {
-    public class JabbRDataProtection : IDataProtector
+    public class JabbRDataProtection : IDataProtectionProvider
     {
         private readonly ICryptoService _cryptoService;
+
         public JabbRDataProtection(ICryptoService cryptoService)
         {
             _cryptoService = cryptoService;
         }
 
-        public byte[] Protect(byte[] userData)
+        public IDataProtector CreateProtector(string purpose)
         {
-            return _cryptoService.Protect(userData);
+            return new JabbRDataProtector(_cryptoService);
+        }
+    }
+
+    public class JabbRDataProtector : IDataProtector
+    {
+        private readonly ICryptoService _cryptoService;
+
+        public JabbRDataProtector(ICryptoService cryptoService)
+        {
+            _cryptoService = cryptoService;
+        }
+
+        public IDataProtector CreateProtector(string purpose)
+        {
+            return this;
+        }
+
+        public byte[] Protect(byte[] plaintext)
+        {
+            return _cryptoService.Protect(plaintext);
         }
 
         public byte[] Unprotect(byte[] protectedData)
