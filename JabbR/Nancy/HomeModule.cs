f9f775ec90f91760f97288b2ae4734165d6d19b8
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:13:08 +0000

    [Commit] Iteration 51 #errors = 15433 <= 206.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/HomeModule.cs][CS0234][15,17] The type or namespace name 'Security' does not exist in the namespace 'Microsoft' (are you missing an assembly reference?)``.

--- JabbR/Nancy/HomeModule.cs
+++ JabbR/Nancy/HomeModule.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -12,7 +12,7 @@ using JabbR.Services;
 using JabbR.UploadHandlers;
 using JabbR.ViewModels;
 using Microsoft.AspNet.SignalR.Infrastructure;
-using Microsoft.Security.Application;
+using System.Web;
 using Nancy;
 
 namespace JabbR.Nancy
@@ -203,7 +203,7 @@ namespace JabbR.Nancy
                 }
             }
 
-            return String.Join(",", resourcesToEmbed.Select(e => string.Format("'{0}': {1}", e.Key, Encoder.JavaScriptEncode(e.Value))));
+return String.Join(",", resourcesToEmbed.Select(e => string.Format("'{0}': {1}", e.Key, HttpUtility.JavaScriptStringEncode(e.Value))));
         }
     }
 }
