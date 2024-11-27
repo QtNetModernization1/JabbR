ead74fcaeba10c2796f7a8bbf37eae0e1b500e1d
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:26:32 +0000

    [Commit] Iteration 43 #errors = 116 <= 116.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/InputExtensions.cs][CS0246][9,56] The type or namespace name 'HtmlHelpers<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/InputExtensions.cs
+++ JabbR/Nancy/InputExtensions.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Linq;
+using Nancy;
 using Nancy.ViewEngines.Razor;
 
 namespace JabbR
@@ -18,7 +19,7 @@ namespace JabbR
 
         public static IHtmlString TextBox<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName, string className, string placeholder)
         {
-            return InputHelper(htmlHelper, "text", propertyName, htmlHelper.GetValueForProperty(propertyName), className, placeholder);
+            return InputHelper(htmlHelper, "text", propertyName, GetValueForProperty(htmlHelper, propertyName), className, placeholder);
         }
 
         public static IHtmlString Password<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
@@ -39,15 +40,15 @@ namespace JabbR
         private const string InputTemplate = @"<input type=""{0}"" id=""{1}"" name=""{2}"" value=""{3}"" class=""{4}"" placeholder=""{5}"" />";
         private static IHtmlString InputHelper<TModel>(HtmlHelpers<TModel> htmlHelper, string inputType, string propertyName, string value, string className, string placeholder)
         {
-            bool hasError = htmlHelper.GetErrorsForProperty(propertyName).Any();
+            bool hasError = htmlHelper.HasErrors(propertyName);
 
             return new NonEncodedHtmlString(String.Format(InputTemplate, inputType, propertyName, propertyName, value, hasError ? String.Format("{0} {1}", className, "error").Trim() : className, placeholder));
         }
 
-        internal static string GetValueForProperty<TModel>(this HtmlHelpers<TModel> htmlHelper, string propertyName)
+        internal static string GetValueForProperty<TModel>(HtmlHelpers<TModel> htmlHelper, string propertyName)
         {
             var propInfo =
-                typeof (TModel).GetProperties()
+                typeof(TModel).GetProperties()
                                .FirstOrDefault(
                                    x => x.Name.Equals(propertyName, StringComparison.InvariantCultureIgnoreCase));
 
@@ -60,12 +61,12 @@ namespace JabbR
 
             if (String.IsNullOrWhiteSpace(value))
             {
-                value = htmlHelper.RenderContext.Context.Request.Form[propertyName];
+                value = htmlHelper.Context.Request.Form[propertyName];
             }
 
             if (String.IsNullOrWhiteSpace(value))
             {
-                value = htmlHelper.RenderContext.Context.Request.Query[propertyName];
+                value = htmlHelper.Context.Request.Query[propertyName];
             }
 
             return value;
