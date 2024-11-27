ad7ebf2e5d4749357a1517e6d94eb46854871704
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:55:29 +0000

    [Commit] Iteration 2 #errors = 15434 <= 206.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Commands/CommandManager.cs][CS0234][3,29] The type or namespace name 'Composition' does not exist in the namespace 'System.ComponentModel' (are you missing an assembly reference?)``.

--- JabbR/Commands/CommandManager.cs
+++ JabbR/Commands/CommandManager.cs
@@ -1,6 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.Composition.Hosting;
+using System.Composition.Hosting;
 using System.Linq;
 using System.Text.RegularExpressions;
 using JabbR.Models;
