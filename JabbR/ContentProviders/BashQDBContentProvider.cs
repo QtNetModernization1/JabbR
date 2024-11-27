b2643ac06c4f7773d3fea866893392eaf56b68ab
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:17:53 +0000

    Revert "[Commit] Iteration 127 #errors = 71 <= 70."
    This reverts commit 48f75a33b0aeb3a4b87f9e15d1aa60b069b15135.

--- JabbR/ContentProviders/BashQDBContentProvider.cs
+++ JabbR/ContentProviders/BashQDBContentProvider.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
@@ -19,19 +19,21 @@ namespace JabbR.ContentProviders
 
         private static readonly string[] WhiteListHtml = new[] { "br", "#text" };
 
-        protected override async Task<ContentProviderResult?> GetCollapsibleContent(ContentProviderHttpRequest request)
+        protected override Task<ContentProviderResult> GetCollapsibleContent(ContentProviderHttpRequest request)
         {
-            var pageInfo = await ExtractFromResponse(request);
-            if (pageInfo == null)
+            return ExtractFromResponse(request).Then(pageInfo =>
             {
-                return null;
-            }
+                if (pageInfo == null)
+                {
+                    return null;
+                }
 
-            return new ContentProviderResult
-            {
-                Content = String.Format(ContentFormat, pageInfo.PageURL, pageInfo.QuoteNumber, pageInfo.Quote),
-                Title = pageInfo.PageURL
-            };
+                return new ContentProviderResult
+                {
+                    Content = String.Format(ContentFormat, pageInfo.PageURL, pageInfo.QuoteNumber, pageInfo.Quote),
+                    Title = pageInfo.PageURL
+                };
+            });
         }
 
         private Task<PageInfo> ExtractFromResponse(ContentProviderHttpRequest request)
