ace050fac34730138957f89b787e02dd25c8289a
NoReply Amazon <no-reply@amazon.com>
2024-11-27 20:44:04 +0000

    Revert "[Commit] Iteration 74 #errors = 94 <= 94."
    This reverts commit e547c870b1fbb339ea382fc86c1fe8dac917c808.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -35,11 +35,6 @@ namespace JabbR.Nancy
             return new DefaultNancyEnvironment();
         }
 
-        protected override INancyEnvironmentConfigurator GetEnvironmentConfigurator()
-        {
-            return NancyInternalConfiguration.Default;
-        }
-
         protected override void ApplicationStartup(IKernel container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
