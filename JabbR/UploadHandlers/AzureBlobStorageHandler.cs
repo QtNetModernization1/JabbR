4779f6d3253588cc5ba74ee5d59d5cc847022931
NoReply Amazon <no-reply@amazon.com>
2024-11-27 21:19:25 +0000

    [Commit] Iteration 130 #errors = 63 <= 67.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/UploadHandlers/AzureBlobStorageHandler.cs][CS1061][46,27] 'CloudBlobContainer' does not contain a definition for 'CreateIfNotExists' and no accessible extension method 'CreateIfNotExists' accepting a first argument of type 'CloudBlobContainer' could be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/UploadHandlers/AzureBlobStorageHandler.cs
+++ JabbR/UploadHandlers/AzureBlobStorageHandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.ComponentModel.Composition;
 using System.IO;
 using System.Threading.Tasks;
@@ -43,10 +43,10 @@ namespace JabbR.UploadHandlers
                                 "_" +
                                 Guid.NewGuid().ToString().Substring(0, 4) + Path.GetExtension(fileName);
 
-            if (container.CreateIfNotExists())
+            if (await container.CreateIfNotExistsAsync())
             {
                 // We need this to make files servable from blob storage
-                container.SetPermissions(new BlobContainerPermissions
+                await container.SetPermissionsAsync(new BlobContainerPermissions
                 {
                     PublicAccess = BlobContainerPublicAccessType.Blob
                 });
@@ -55,7 +55,7 @@ namespace JabbR.UploadHandlers
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(randomFile);
             blockBlob.Properties.ContentType = contentType;
 
-            await Task.Factory.FromAsync((cb, state) => blockBlob.BeginUploadFromStream(stream, cb, state), ar => blockBlob.EndUploadFromStream(ar), null);
+            await blockBlob.UploadFromStreamAsync(stream);
 
             var result = new UploadResult
             {
