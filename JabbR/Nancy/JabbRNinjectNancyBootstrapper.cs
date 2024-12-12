0fa947d625dbd00ed9922f8647eaa28cdad0e74f
NoReply Amazon <no-reply@amazon.com>
2024-12-12 23:57:42 +0000

    [Commit] Iteration 238 #errors = 113 <= 114.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs][CS0029][62,43] Cannot implicitly convert type 'JabbR.Nancy.ClaimsPrincipalUserIdentity' to 'System.Security.Claims.ClaimsPrincipal'``.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -59,7 +59,7 @@ namespace JabbR.Nancy
                 var principal = Get<IPrincipal>(env, "server.User") as ClaimsPrincipal;
                 if (principal != null)
                 {
-                    context.CurrentUser = new ClaimsPrincipalUserIdentity(principal);
+                    context.CurrentUser = principal;
                 }
 
                 var appMode = Get<string>(env, "host.AppMode");
