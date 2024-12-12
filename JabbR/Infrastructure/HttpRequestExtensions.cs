7fdfb84b039724d52abdbe41b414fe615f215cc7
NoReply Amazon <no-reply@amazon.com>
2024-12-12 23:11:28 +0000

    [Commit] Iteration 190 #errors = 164 <= 166.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/HttpRequestExtensions.cs][CS1061][63,43] 'HttpRequestMessage' does not contain a definition for 'CreateResponse' and no accessible extension method 'CreateResponse' accepting a first argument of type 'HttpRequestMessage' could be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Infrastructure/HttpRequestExtensions.cs
+++ JabbR/Infrastructure/HttpRequestExtensions.cs
@@ -60,10 +60,11 @@ namespace JabbR.Infrastructure
         /// <returns>HttpResponseMessage that wraps the given payload</returns>
         public static HttpResponseMessage CreateJabbrErrorMessage(this HttpRequestMessage request, HttpStatusCode statusCode, string message, string filenamePrefix)
         {
-            var responseMessage = request.CreateResponse(
-                statusCode, 
-                new ErrorModel { Message = message }, 
-                new MediaTypeHeaderValue("application/json"));
+            var responseMessage = new HttpResponseMessage(statusCode)
+            {
+                Content = JsonContent.Create(new ErrorModel { Message = message })
+            };
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
             return AddResponseHeaders(request, responseMessage, filenamePrefix);
         }
@@ -76,10 +77,11 @@ namespace JabbR.Infrastructure
         /// <returns>HttpResponseMessage that wraps the given payload</returns>
         public static HttpResponseMessage CreateJabbrErrorMessage(this HttpRequestMessage request, HttpStatusCode statusCode, string message)
         {
-            var responseMessage = request.CreateResponse(
-                statusCode, 
-                new ErrorModel { Message = message }, 
-                new MediaTypeHeaderValue("application/json"));
+            var responseMessage = new HttpResponseMessage(statusCode)
+            {
+                Content = JsonContent.Create(new ErrorModel { Message = message })
+            };
+            responseMessage.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
             return AddResponseHeaders(request, responseMessage, null);
         }
