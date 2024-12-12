4f575fb4195ffde24c80f87c434ab29179aedbbd
NoReply Amazon <no-reply@amazon.com>
2024-12-12 22:07:56 +0000

    Revert "[Commit] Iteration 106 #errors = 210 <= 211."
    This reverts commit ba77bd5a7afdf55dc0f36cd9004ace4caf6a8404.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -1,20 +1,20 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
+
 using JabbR.Services;
+
 using Nancy;
 using Nancy.ErrorHandling;
-using Nancy.Responses;
 using Nancy.ViewEngines;
 
 namespace JabbR.Nancy
 {
-    public class ErrorPageHandler : IStatusCodeHandler
+    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
     {
         private readonly IJabbrRepository _repository;
-        private readonly IViewFactory _viewFactory;
 
-        public ErrorPageHandler(IViewFactory viewFactory, IJabbrRepository repository)
+        public ErrorPageHandler(IViewFactory factory, IJabbrRepository repository)
+            : base(factory)
         {
-            _viewFactory = viewFactory;
             _repository = repository;
         }
 
@@ -40,15 +40,18 @@ namespace JabbR.Nancy
                 }
             }
 
-            var viewModel = new
-            {
-                Error = statusCode,
-                ErrorCode = (int)statusCode,
-                SuggestRoomName = suggestRoomName
-            };
+            var response = RenderView(
+                context, 
+                "errorPage", 
+                new 
+                { 
+                    Error = statusCode,
+                    ErrorCode = (int)statusCode,
+                    SuggestRoomName = suggestRoomName
+                });
 
-            var view = _viewFactory.RenderView("errorPage", viewModel);
-            context.Response = new HtmlResponse(statusCode, view);
+            response.StatusCode = statusCode;
+            context.Response = response;
         }
     }
 }
