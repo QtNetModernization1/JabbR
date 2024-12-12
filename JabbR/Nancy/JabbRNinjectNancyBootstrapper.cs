a4b211490cfc7856c8338e0bddc329b84c5f7f87
NoReply Amazon <no-reply@amazon.com>
2024-12-12 22:18:07 +0000

    Revert "[Commit] Iteration 127 #errors = 202 <= 202."
    This reverts commit 5e4922b985a4aab9bb921f3fa277e4e48dfc2166.

--- JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
+++ JabbR/Nancy/JabbRNinjectNancyBootstrapper.cs
@@ -35,11 +35,6 @@ namespace JabbR.Nancy
             return new DefaultNancyEnvironment();
         }
 
-        protected override INancyEnvironmentConfigurator GetEnvironmentConfigurator()
-        {
-            return environment => { };
-        }
-
         protected override void RegisterNancyEnvironment(IKernel container, INancyEnvironment environment)
         {
             // Register the Nancy environment
