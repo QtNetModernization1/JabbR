5bb57b4a70a7b0609a07bfcaca918768cf8ec779
NoReply Amazon <no-reply@amazon.com>
2024-12-12 22:56:24 +0000

    [Commit] Iteration 164 #errors = 185 <= 186.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ErrorPageHandler.cs][CS1729][16,15] 'DefaultViewRenderer' does not contain a constructor that takes 1 arguments``.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.RegularExpressions;
+using System.Text.RegularExpressions;
 
 using JabbR.Services;
 
@@ -8,13 +8,14 @@ using Nancy.ViewEngines;
 
 namespace JabbR.Nancy
 {
-    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
+    public class ErrorPageHandler : IStatusCodeHandler
     {
+        private readonly IViewRenderer _viewRenderer;
         private readonly IJabbrRepository _repository;
 
-        public ErrorPageHandler(IViewFactory factory, IJabbrRepository repository)
-            : base(factory)
+        public ErrorPageHandler(IViewRenderer viewRenderer, IJabbrRepository repository)
         {
+            _viewRenderer = viewRenderer;
             _repository = repository;
         }
 
@@ -40,15 +41,15 @@ namespace JabbR.Nancy
                 }
             }
 
-            var response = RenderView(
-                context, 
-                "errorPage", 
-                new 
-                { 
+            var response = _viewRenderer.RenderView(
+                "errorPage",
+                new
+                {
                     Error = statusCode,
                     ErrorCode = (int)statusCode,
                     SuggestRoomName = suggestRoomName
-                });
+                },
+                context);
 
             response.StatusCode = statusCode;
             context.Response = response;
