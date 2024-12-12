c4d5cace48db3326d4a0aeb0dff91f9dff08ad33
NoReply Amazon <no-reply@amazon.com>
2024-12-12 21:31:27 +0000

    [Commit] Iteration 61 #errors = 272 <= 307.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Services/ChatService.cs][CS0234][7,17] The type or namespace name 'AspNet' does not exist in the namespace 'Microsoft' (are you missing an assembly reference?)``.

--- JabbR/Services/ChatService.cs
+++ JabbR/Services/ChatService.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using JabbR.Models;
 using JabbR.UploadHandlers;
-using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.SignalR;
 using Newtonsoft.Json;
 
 namespace JabbR.Services
