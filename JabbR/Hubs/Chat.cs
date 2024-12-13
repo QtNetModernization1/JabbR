7ea44825a006641c36dc3426e03e5441c073542b
NoReply Amazon <no-reply@amazon.com>
2024-12-13 00:15:32 +0000

    [Commit] Iteration 343 #errors = 107 <= 106.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Hubs/Chat.cs][CS1503][531,79] Argument 2: cannot convert from 'Microsoft.AspNetCore.SignalR.IHubCallerClients' to 'Microsoft.AspNetCore.SignalR.IHubContext<Microsoft.AspNetCore.SignalR.Hub>'``.

--- JabbR/Hubs/Chat.cs
+++ JabbR/Hubs/Chat.cs
@@ -10,6 +10,7 @@ using JabbR.Models;
 using JabbR.Services;
 using JabbR.ViewModels;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Infrastructure;
 using Newtonsoft.Json;
 using System.Runtime.CompilerServices;
 
@@ -24,19 +25,19 @@ namespace JabbR
         private readonly IChatService _service;
         private readonly IRecentMessageCache _recentMessageCache;
         private readonly ICache _cache;
-        private readonly ContentProviderProcessor _resourceProcessor;
+private readonly Func<IHubCallerClients, ContentProviderProcessor> _resourceProcessorFactory;
         private readonly ILogger _logger;
         private readonly ApplicationSettings _settings;
 
-        public Chat(ContentProviderProcessor resourceProcessor,
-                    IChatService service,
-                    IRecentMessageCache recentMessageCache,
-                    IJabbrRepository repository,
-                    ICache cache,
-                    ILogger logger,
-                    ApplicationSettings settings)
-        {
-            _resourceProcessor = resourceProcessor;
+    public Chat(Func<IHubCallerClients, ContentProviderProcessor> resourceProcessorFactory,
+                IChatService service,
+                IRecentMessageCache recentMessageCache,
+                IJabbrRepository repository,
+                ICache cache,
+                ILogger logger,
+                ApplicationSettings settings)
+    {
+        _resourceProcessorFactory = resourceProcessorFactory;
             _service = service;
             _recentMessageCache = recentMessageCache;
             _repository = repository;
@@ -527,8 +528,10 @@ public async Task<bool> Send(ClientMessage clientMessage)
                 var urls = UrlExtractor.ExtractUrls(chatMessage.Content);
                 if (urls.Count > 0)
                 {
+                    // Create a new ContentProviderProcessor instance with the current Clients
+                    var resourceProcessor = _resourceProcessorFactory(Clients);
                     // Use Task.Run to run the synchronous method asynchronously
-                    await Task.Run(() => _resourceProcessor.ProcessUrls(urls, Clients, room.Name, chatMessage.Id));
+                    await Task.Run(() => resourceProcessor.ProcessUrls(urls, Context.ConnectionId, room.Name, chatMessage.Id));
                 }
             }
         }
