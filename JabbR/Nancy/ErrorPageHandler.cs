c3b02ce2b726026adad857c0617953705a924d68
NoReply Amazon <no-reply@amazon.com>
2024-12-13 02:09:14 +0000

    Revert "[Commit] Iteration 531 #errors = 12 <= 14."
    This reverts commit e56e7b5c4367b6869c439066bb89432b11a8dff7.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -1,22 +1,20 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 using JabbR.Services;
 
 using Nancy;
 using Nancy.ErrorHandling;
 using Nancy.ViewEngines;
-using Nancy.Responses;
 
 namespace JabbR.Nancy
 {
-    public class ErrorPageHandler : IStatusCodeHandler
+    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
     {
         private readonly IJabbrRepository _repository;
-        private readonly IViewFactory _viewFactory;
 
-        public ErrorPageHandler(IViewFactory viewFactory, IJabbrRepository repository)
+        public ErrorPageHandler(IViewFactory factory, IJabbrRepository repository)
+            : base(factory)
         {
-            _viewFactory = viewFactory;
             _repository = repository;
         }
 
@@ -42,19 +40,17 @@ namespace JabbR.Nancy
                 }
             }
 
-            var model = new
-            {
-                Error = statusCode,
-                ErrorCode = (int)statusCode,
-                SuggestRoomName = suggestRoomName
-            };
+            var response = RenderView(
+                context, 
+                "errorPage", 
+                new 
+                { 
+                    Error = statusCode,
+                    ErrorCode = (int)statusCode,
+                    SuggestRoomName = suggestRoomName
+                });
 
-            var response = new Response
-            {
-                StatusCode = statusCode,
-                Contents = stream => _viewFactory.RenderView("errorPage", model, new ViewLocationContext(context, null)).Contents(stream)
-            };
-
+            response.StatusCode = statusCode;
             context.Response = response;
         }
     }
