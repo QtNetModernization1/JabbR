4a28e832d3194d425cf93f3e4ff1a14eb85602be
NoReply Amazon <no-reply@amazon.com>
2024-12-13 01:02:17 +0000

    [Commit] Iteration 407 #errors = 100 <= 97.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Services/Email/RazorEmailTemplateEngine.cs][CS0411][203,116] The type arguments for method 'Enumerable.SelectMany<TSource, TResult>(IEnumerable<TSource>, Func<TSource, IEnumerable<TResult>>)' cannot be inferred from the usage. Try specifying the type arguments explicitly.``.

--- JabbR/Services/Email/RazorEmailTemplateEngine.cs
+++ JabbR/Services/Email/RazorEmailTemplateEngine.cs
@@ -9,6 +9,7 @@ using System.Threading;
 using Microsoft.AspNetCore.Razor;
 using JabbR.Infrastructure;
 using Microsoft.CSharp;
+using Microsoft.AspNetCore.Razor.CodeGenerators;
 
 namespace JabbR.Services
 {
@@ -200,7 +201,7 @@ namespace JabbR.Services
 
             if (templateResults.Any(result => result.ParserErrors.Any()))
             {
-                var parseExceptionMessage = String.Join(Environment.NewLine + Environment.NewLine, templateResults.SelectMany(r => r.ParserErrors).Select(e => e.Location + ":" + Environment.NewLine + e.Message).ToArray());
+                var parseExceptionMessage = String.Join(Environment.NewLine + Environment.NewLine, templateResults.SelectMany<GeneratorResults, GeneratorError>(r => r.ParserErrors).Select(e => e.Location + ":" + Environment.NewLine + e.Message).ToArray());
 
                 throw new InvalidOperationException(parseExceptionMessage);
             }
