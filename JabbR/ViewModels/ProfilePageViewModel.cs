fde0b6c792566d799a858335c95b40e4f4faf5ad
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:26:16 +0000

    [Commit] Iteration 26 #errors = 136 <= 137.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/ViewModels/ProfilePageViewModel.cs][CS0234][7,28] The type or namespace name 'Core' does not exist in the namespace 'SimpleAuthentication' (are you missing an assembly reference?)``.

--- JabbR/ViewModels/ProfilePageViewModel.cs
+++ JabbR/ViewModels/ProfilePageViewModel.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 using JabbR.Models;
 using JabbR.Services;
-using SimpleAuthentication.Core;
+using SimpleAuthentication;
 
 namespace JabbR.ViewModels
 {
