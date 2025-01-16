217a33c86089074c1b7c5226229ac8246c637d80
NoReply Amazon <no-reply@amazon.com>
2025-01-16 23:18:39 +0000

    Revert "[Commit] Iteration 833 #errors = 51 <= 52."
    This reverts commit 8aa8b4e0d4e9baa0c5c27edb433b16eb69c49a0d.

--- JabbR/Nancy/AccountModule.cs
+++ JabbR/Nancy/AccountModule.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Claims;
 using JabbR.Infrastructure;
 using JabbR.Models;
 using JabbR.Services;
@@ -278,8 +277,7 @@ public class AccountModule : NancyModule
 
                 ValidatePassword(password, confirmPassword);
 
-                var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-                ChatUser user = repository.GetUserById(userId);
+                ChatUser user = repository.GetUserById(Principal.GetUserId());
 
                 try
                 {
@@ -331,8 +329,7 @@ public class AccountModule : NancyModule
 
                 ValidatePassword(password, confirmPassword);
 
-                var userId = _httpContextAccessor.HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-                ChatUser user = repository.GetUserById(userId);
+                ChatUser user = repository.GetUserById(Principal.GetUserId());
 
                 try
                 {
