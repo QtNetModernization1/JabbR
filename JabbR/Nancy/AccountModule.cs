4e8d0ee124f473d2dcbafe96f1eef27e6872c85c
NoReply Amazon <no-reply@amazon.com>
2025-01-16 21:12:33 +0000

    Revert "[Commit] Iteration 306 #errors = 124 <= 123."
    This reverts commit 9e2b492500d5cedaed0bb04e9d0e2401b7ca46b3.

--- JabbR/Nancy/AccountModule.cs
+++ JabbR/Nancy/AccountModule.cs
@@ -10,7 +10,7 @@ using JabbR.ViewModels;
 
 namespace JabbR.Nancy
 {
-    public class AccountModule : NancyModule
+    public class AccountModule
     {
         public AccountModule(ApplicationSettings applicationSettings,
                              IMembershipService membershipService,
@@ -19,8 +19,8 @@ namespace JabbR.Nancy
                              IChatNotificationService notificationService,
                              IUserAuthenticator authenticator,
                              IEmailService emailService)
+            : base("/account")
         {
-            this.ModulePath = "/account";
             Get["/"] = _ =>
             {
                 if (!IsAuthenticated)
