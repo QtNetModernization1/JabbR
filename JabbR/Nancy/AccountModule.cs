2ef7e4cd11d0899cd8d08dbfa23856d3334b68df
NoReply Amazon <no-reply@amazon.com>
2025-01-16 21:57:50 +0000

    Revert "[Commit] Iteration 373 #errors = 84 <= 84."
    This reverts commit 6c82a6a8a0ad29609775e00570b9093777815340.

--- JabbR/Nancy/AccountModule.cs
+++ JabbR/Nancy/AccountModule.cs
@@ -133,7 +133,7 @@ public class AccountModule : NancyModule
                 return View["register"];
             });
 
-            Post["/create"] = parameters =>
+            Post["/create"] = _ =>
             {
                 if (!HasValidCsrfTokenOrSecHeader)
                 {
