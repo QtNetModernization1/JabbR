d8e21a6d3fb8eb43fe4c639188d15cc9fc788508
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:38:00 +0000

    Revert "[Commit] Iteration 39 #errors = 132 <= 133."
    This reverts commit f83a3f0de643eb7cc34fa7ba9337bf91cfc526e9.

--- JabbR/Nancy/ErrorPageHandler.cs
+++ JabbR/Nancy/ErrorPageHandler.cs
@@ -1,4 +1,4 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 using JabbR.Services;
 
@@ -8,14 +8,13 @@ using Nancy.ViewEngines;
 
 namespace JabbR.Nancy
 {
-    public class ErrorPageHandler : IStatusCodeHandler
+    public class ErrorPageHandler : DefaultViewRenderer, IStatusCodeHandler
     {
         private readonly IJabbrRepository _repository;
-        private readonly IViewFactory _viewFactory;
 
-        public ErrorPageHandler(IViewFactory viewFactory, IJabbrRepository repository)
+        public ErrorPageHandler(IViewFactory factory, IJabbrRepository repository)
+            : base(factory)
         {
-            _viewFactory = viewFactory;
             _repository = repository;
         }
 
@@ -41,24 +40,17 @@ namespace JabbR.Nancy
                 }
             }
 
-            var viewLocationResult = _viewFactory.FindView(context, "errorPage");
-            var response = new Response
-            {
-                Contents = stream =>
-                {
-                    var renderContext = new DefaultRenderContext(context);
-                    viewLocationResult.Contents(renderContext.ViewCache).Invoke(stream,
-                        new
-                        {
-                            Error = statusCode,
-                            ErrorCode = (int)statusCode,
-                            SuggestRoomName = suggestRoomName
-                        });
-                },
-                ContentType = "text/html",
-                StatusCode = statusCode
-            };
+            var response = RenderView(
+                context, 
+                "errorPage", 
+                new 
+                { 
+                    Error = statusCode,
+                    ErrorCode = (int)statusCode,
+                    SuggestRoomName = suggestRoomName
+                });
 
+            response.StatusCode = statusCode;
             context.Response = response;
         }
     }
