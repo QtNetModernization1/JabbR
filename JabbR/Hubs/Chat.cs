779d14efe1311b31b8eb6458de3231104b39b0ed
NoReply Amazon <no-reply@amazon.com>
2025-01-16 23:55:31 +0000

    [Commit] Iteration 904 #errors = 8 <= 9.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Hubs/Chat.cs][CS1929][1231,21] 'IGroupManager' does not contain a definition for 'Remove' and the best extension method overload 'CollectionExtensions.Remove<string, string>(IDictionary<string, string>, string, out string)' requires a receiver of type 'System.Collections.Generic.IDictionary<string, string>'``.

--- JabbR/Hubs/Chat.cs
+++ JabbR/Hubs/Chat.cs
@@ -1216,11 +1216,11 @@ private async Task JoinRoomAsync(ChatUser user, ChatRoom room)
             {
                 reason = null;
             }
-            
+
             // We send down room so that other clients can display that the user has been banned
             foreach (var room in rooms)
             {
-                Clients.Group(room).SendAsync("ban", targetUserViewModel, room, callingUserViewModel, reason);
+                Clients.Group(room).SendAsync("ban", targetUserViewModel, room, callingUserViewModel, reason).Wait();
             }
 
             foreach (var client in targetUser.ConnectedClients)
@@ -1228,7 +1228,7 @@ private async Task JoinRoomAsync(ChatUser user, ChatRoom room)
                 foreach (var room in rooms)
                 {
                     // Remove the user from this the room group so he doesn't get the general ban message
-                    Groups.Remove(client.Id, room);
+                    Groups.RemoveFromGroupAsync(client.Id, room).Wait();
                 }
             }
         }
