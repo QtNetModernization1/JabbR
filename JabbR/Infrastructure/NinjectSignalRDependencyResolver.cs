f98ac3711f5351dd2aad37191b7a1b08e9cde26a
NoReply Amazon <no-reply@amazon.com>
2025-01-16 19:35:21 +0000

    [Commit] Iteration 141 #errors = 223 <= 221.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Infrastructure/NinjectSignalRDependencyResolver.cs][CS0246][9,55] The type or namespace name 'DefaultDependencyResolver' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Infrastructure/NinjectSignalRDependencyResolver.cs
+++ JabbR/Infrastructure/NinjectSignalRDependencyResolver.cs
@@ -6,22 +6,34 @@ using Ninject;
 
 namespace JabbR.Infrastructure
 {
-    internal class NinjectSignalRDependencyResolver : DefaultDependencyResolver
+    internal class NinjectSignalRDependencyResolver : IHubActivator
     {
         private readonly IKernel _kernel;
+
         public NinjectSignalRDependencyResolver(IKernel kernel)
         {
             _kernel = kernel;
         }
 
-        public override object GetService(Type serviceType)
+        public HubLifetimeContext Create(HubActivatorContext context)
         {
-            return _kernel.TryGet(serviceType) ?? base.GetService(serviceType);
+            return new HubLifetimeContext(context.Context, _kernel.Get(context.HubType), (hub, isDisposable) =>
+            {
+                if (isDisposable)
+                {
+                    _kernel.Release(hub);
+                }
+            });
         }
 
-        public override IEnumerable<object> GetServices(Type serviceType)
+        public T GetService<T>() where T : class
         {
-            return _kernel.GetAll(serviceType).Concat(base.GetServices(serviceType));
+            return _kernel.TryGet<T>();
+        }
+
+        public IEnumerable<T> GetServices<T>() where T : class
+        {
+            return _kernel.GetAll<T>();
         }
     }
 }
