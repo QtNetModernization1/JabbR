9b2aebb0512fe88245d268155f44ceaa8969a022
NoReply Amazon <no-reply@amazon.com>
2025-01-16 18:31:38 +0000

    [Commit] Iteration 95 #errors = 385 <= 394.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Models/RepositoryExtensions.cs][CS0234][7,17] The type or namespace name 'AspNet' does not exist in the namespace 'Microsoft' (are you missing an assembly reference?)``.

--- JabbR/Models/RepositoryExtensions.cs
+++ JabbR/Models/RepositoryExtensions.cs
@@ -1,10 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using JabbR.Services;
 using JabbR.Infrastructure;
-using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.SignalR;
 
 namespace JabbR.Models
 {
