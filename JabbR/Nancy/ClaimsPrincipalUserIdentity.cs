403b0b0cde76b0d2b56ccd73aecbc883e6f9ce6f
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:35:48 +0000

    Revert "[Commit] Iteration 35 #errors = 136 <= 133."
    This reverts commit c58308d1000acdb23aca34271caa226538f967e6.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,6 +1,5 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Security.Claims;
-using Nancy;
 using Nancy.Security;
 
 namespace JabbR.Nancy
@@ -10,14 +9,20 @@ namespace JabbR.Nancy
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
-            UserName = claimsPrincipal.Identity?.Name ?? string.Empty;
-            Claims = claimsPrincipal.Claims.Select(c => c.Value);
         }
 
-        public ClaimsPrincipal ClaimsPrincipal { get; }
+        public ClaimsPrincipal ClaimsPrincipal { get; private set; }
 
-        public IEnumerable<string> Claims { get; }
+        public IEnumerable<string> Claims
+        {
+            get;
+            set;
+        }
 
-        public string UserName { get; }
+        public string UserName
+        {
+            get;
+            set;
+        }
     }
 }
