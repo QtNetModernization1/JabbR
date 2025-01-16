1cb8297f2730fd0a500b536b359e5d8e59479b0e
NoReply Amazon <no-reply@amazon.com>
2025-01-16 17:36:13 +0000

    [Commit] Iteration 36 #errors = 133 <= 133.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/JabbR/Nancy/ClaimsPrincipalUserIdentity.cs][CS0246][7,48] The type or namespace name 'IUserIdentity' could not be found (are you missing a using directive or an assembly reference?)``.

--- JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
+++ JabbR/Nancy/ClaimsPrincipalUserIdentity.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using Nancy.Security;
 
@@ -9,20 +10,14 @@ namespace JabbR.Nancy
         public ClaimsPrincipalUserIdentity(ClaimsPrincipal claimsPrincipal)
         {
             ClaimsPrincipal = claimsPrincipal;
+            UserName = claimsPrincipal.Identity?.Name ?? string.Empty;
+            Claims = claimsPrincipal.Claims.Select(c => c.Value).ToList();
         }
 
-        public ClaimsPrincipal ClaimsPrincipal { get; private set; }
+        public ClaimsPrincipal ClaimsPrincipal { get; }
 
-        public IEnumerable<string> Claims
-        {
-            get;
-            set;
-        }
+        public IEnumerable<string> Claims { get; }
 
-        public string UserName
-        {
-            get;
-            set;
-        }
+        public string UserName { get; }
     }
 }
